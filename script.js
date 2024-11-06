b669e1052cfbc4cf3461dfab495d0f90d6152e6e
Gamer2CoderYT <gamertoocoder@gmail.com>
2024-11-06 19:29:11 +0000

    Validate Input function

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const placeholder = document.getElementById("placeholder");
 const addEventBtn = document.getElementById("add-event-btn");
 const addEventContainer = document.getElementById("add-event-container");
 const saveBtn = document.getElementById("save-btn");
+const eventNameInput = document.getElementById("event-name");
+const eventDescriptionInput = document.getElementById("event-description");
+const eventDateInput = document.getElementById("event-date"); 
 
 
 
@@ -13,10 +16,33 @@ const saveBtn = document.getElementById("save-btn");
 const addEventForm = () => {
   homeContainer.classList.toggle("hidden");
   addEventContainer.classList.toggle("hidden");
+  saveBtn.addEventListener("click", validateInput);
 }
 
 // Make the addEventBtn call the addEventForm function
 addEventBtn.addEventListener("click", addEventForm);
+
+// Validate event form input
+const validateInput = ()=>{
+  // Stores the values of inputs as variables
+  const eventName = eventNameInput.value;
+  const eventDesc = eventDescriptionInput.value;
+  const eventDate = eventDateInput.value;
+  const todaysDate = new Date ();
+  const eventDateObj = new Date (eventDate);
+
+  // Checks if any of the inputs are empty and if they are alerts the user
+  if (!(eventName && eventDesc && eventDate)){
+    alert("Please fill in all required fields.");
+  } 
+  // Checks the date entered is not before the current date
+  else if (eventDateObj < todaysDate) {
+    alert("Date of event must be in the future.");
+  }
+  else {
+    alert("All fields passed");
+  }
+}
 
 // Create Event Object
 
