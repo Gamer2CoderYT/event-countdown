61e31b225865046afda2a083941bf44888b3eb8a
Gamer2CoderYT <gamertoocoder@gmail.com>
2024-11-06 18:36:42 +0000

    Function to display the "Add Event" form

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 // Assign HTML elements to variables
 const container = document.getElementById("container");
+const homeContainer = document.getElementById("home-container");
 const eventsContainer = document.getElementById("events-container");
 const placeholder = document.getElementById("placeholder");
 const addEventBtn = document.getElementById("add-event-btn");
+const addEventContainer = document.getElementById("add-event-container");
+const saveBtn = document.getElementById("save-btn");
+
+
+
 // Add event form
+const addEventForm = () => {
+  homeContainer.classList.toggle("hidden");
+  addEventContainer.classList.toggle("hidden");
+}
+
+// Make the addEventBtn call the addEventForm function
+addEventBtn.addEventListener("click", addEventForm);
 
 // Create Event Object
 
